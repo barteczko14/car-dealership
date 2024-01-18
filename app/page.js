e75110eec8aaf655b2837bae775f9f5916a15677
Bartek <bartek.kryczka@wp.pl>
2024-01-18 12:18:07 +0100

    changing styles

--- app/page.js
+++ app/page.js
@@ -2,15 +2,17 @@ import AboutUs from '@/components/AboutUs'
 import Hero from '@/components/Hero'
 import Services from '@/components/Services'
 import Opinion from '@/components/Opinion'
-import React from 'react'
+import classes from './page.module.css'
 
 export default function Home() {
 	return (
 		<>
 			<Hero></Hero>
-			<AboutUs></AboutUs>
-			<Services></Services>
-			<Opinion></Opinion>
+			<div className={classes.wrapper}>
+				<AboutUs></AboutUs>
+				<Services></Services>
+				<Opinion></Opinion>
+			</div>
 		</>
 	)
 }
