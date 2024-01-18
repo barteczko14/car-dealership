fac02e571ca23f1228c3a00f3ab13182b961e0ae
Bartek <bartek.kryczka@wp.pl>
2024-01-18 13:06:17 +0100

    refactoring a hero component

--- components/Hero.js
+++ components/Hero.js
@@ -1,17 +1,17 @@
 import Image from 'next/image'
 import heroImg from '../assets/hero.jpg'
-import heroImg2 from '../assets/hero_2.jpg'
+import heroImg2 from '../assets/hero2.jpg'
 import classes from './Hero.module.css'
 
 const Hero = () => {
 	return (
-		<section id='o-nas' className={classes.hero_wrapper}>
-			<Image className={classes.hero_img} src={heroImg}></Image>
-			<div className={classes.middle}>
-				<Image priority className={classes.middle_img} src={heroImg2}></Image>
-				<h2 className={classes.hero_title}>
-					Your future <span className={classes.color_title}>car</span>
-				</h2>
+		<section id='o-nas' className={classes.hero}>
+			<Image className={classes.outer_img} src={heroImg}></Image>
+			<div>
+				<Image className={classes.inner_img} src={heroImg2}></Image>
+				<h1>
+					Your dream <span>car</span>
+				</h1>
 			</div>
 		</section>
 	)
