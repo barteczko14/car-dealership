0cea370943e5144e1635fcac1f0d38b99d6e50c7
Bartek <bartek.kryczka@wp.pl>
2024-02-08 14:36:21 +0100

    refactoring code

--- components/services/Services.js
+++ components/services/Services.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import classes from './Services.module.css'
 import Title from '../Title'
 import { FaCar } from 'react-icons/fa6'
@@ -13,7 +12,7 @@ const Services = () => {
 					<div className={`${classes.service} ${classes.color_one}`}>
 						<FaCar className={classes.icon} />
 						<h4 className={classes.service_title}>Autohandel</h4>
-						<p className={classes.service_description}>Lorem ipsum dolor sit consect adipiscing.</p>
+						<p className={classes.service_description}>Odkryj świat doskonałych pojazdów z naszego autohandlu.</p>
 						<FaArrowRight className={classes.arrow_icon} />
 					</div>
 				</Link>
@@ -21,7 +20,9 @@ const Services = () => {
 					<div className={`${classes.service} ${classes.color_two}`}>
 						<FaDollarSign className={classes.icon} />
 						<h4 className={classes.service_title}>Sprzedaż części</h4>
-						<p className={classes.service_description}>Lorem ipsum dolor sit consect adipiscing.</p>
+						<p className={classes.service_description}>
+							Znajdź wysokiej jakości części, które utrzymają Twoje auto w doskonałej formie.
+						</p>
 						<FaArrowRight className={classes.arrow_icon} />
 					</div>
 				</Link>
@@ -29,7 +30,9 @@ const Services = () => {
 					<div className={`${classes.service} ${classes.color_three}`}>
 						<FaTools className={classes.icon} />
 						<h4 className={classes.service_title}>Serwis</h4>
-						<p className={classes.service_description}>Lorem ipsum dolor sit consect adipiscing.</p>
+						<p className={classes.service_description}>
+							Zaufaj naszym specjalistom do dbania o kondycję Twojego pojazdu.
+						</p>
 						<FaArrowRight className={classes.arrow_icon} />
 					</div>
 				</Link>
@@ -37,7 +40,9 @@ const Services = () => {
 					<div className={`${classes.service} ${classes.color_four}`}>
 						<FaTruck className={classes.icon} />
 						<h4 className={classes.service_title}>Transport</h4>
-						<p className={classes.service_description}>Lorem ipsum dolor sit consect adipiscing.</p>
+						<p className={classes.service_description}>
+							Szybki i bezpieczny transport pojazdów i maszyn.
+						</p>
 						<FaArrowRight className={classes.arrow_icon} />
 					</div>
 				</Link>
