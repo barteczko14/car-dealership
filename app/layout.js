44fcf2ebca357c49d3a7b34591fac31e97704ccb
Bartek <bartek.kryczka@wp.pl>
2024-02-09 13:22:35 +0100

    changing website title

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import Navbar from '../components/navbar/Navbar'
 import Footer from '@/components/footer/Footer'
 
 export const metadata = {
-	title: 'Cars dealership',
+	title: 'YourDreamCar',
 	description:
 		'Znajdź idealny samochód w naszym profesjonalnym komisie samochodowym. Oferujemy szeroki wybór pojazdów używanych w doskonałym stanie technicznym. Sprawdź naszą ofertę i znajdź swój wymarzony samochód już dziś. Gwarancja jakości, konkurencyjne ceny i przyjazna obsługa klienta.',
 }
