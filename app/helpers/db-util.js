8637fe638b42f736702289dae5d04fc2ea070e72
Bartek <bartek.kryczka@wp.pl>
2024-03-06 09:28:00 +0100

    adding new mongodb account

--- app/helpers/db-util.js
+++ app/helpers/db-util.js
@@ -2,12 +2,8 @@ import { MongoClient } from 'mongodb'
 
 export async function connectDatabase() {
 	const client = await MongoClient.connect(
-		'mongodb+srv://barteczko14:3KBSNUyywZ2nfxQx@cluster0.feqsk3v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+		'mongodb+srv://yourdreamcar:rMtpO7Yam8CGHmKc@cluster0.feqsk3v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 	)
 
 	return client
 }
-
-
-
-
